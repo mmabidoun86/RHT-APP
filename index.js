e3dcb05c301bee527180e68fc9700d2640015654
mmabiodun86@gmail.com <mmabiodun86@gmail.com>
2023-09-16 23:15:08 +0100

    my first self commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const http = require('node:http');
+
+const hostname = '127.0.0.1';
+const port = 1237;
+
+const server = http.createServer((req, res) => {
+  console.log("how far my dear customer")
+  console.log("welcome to my server here you are going to the printing world");
+  res.setHeader("Content-Type", "application/json");
+  res.end({
+    details: "Hello belove client welcome to my world"
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+}); 
---
+++ logs.js
@@ -0,0 +1,14 @@
+function Addition(a, b) {
+    return a + b
+}
+
+console.log (5,7);
+
+
+const ADDITION = (a, b) => {
+    return a + b
+};
+
+console.log (3,90);
+
+module.export 
