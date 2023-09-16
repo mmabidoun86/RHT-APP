965121fac31e0f465473eb353a3413b78e86fbe5
mmabiodun86@gmail.com <mmabiodun86@gmail.com>
2023-09-16 23:47:19 +0100

    msg

--- index.js
+++ index.js
@@ -4,14 +4,14 @@ const hostname = '127.0.0.1';
 const port = 1237;
 
 const server = http.createServer((req, res) => {
-  console.log("how far my dear customer")
+  // console.log("how far my dear customer")
   console.log("welcome to my server here you are going to the printing world");
   res.setHeader("Content-Type", "application/json");
   res.end({
-    details: "Hello belove client welcome to my world"
+    details: "Hello belove client welcome to my world",
   });
 });
 
 server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+  console.log(`It is the new project of self learning`);
 }); 
